6b726087d652fb4d2687d537fd9e662c93189ad6
Ron Ganguly <c1ph3r9@gmail.com>
2018-12-28 06:20:57 -0600

    implementing mongo/mongoose for the blog site to persist posts

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const _ = require('lodash')
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser:true})
+
+const postSchema={
+  title:String,
+  content:String
+}
+
+const Post=mongoose.model("Post",postSchema)
+
+
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,12 +30,21 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-let posts = [];
+// let posts = [];
+
+
+
 app.get("/", (req, res) => {
-  res.render("home", {
-    content: homeStartingContent,
-    newPosts: posts
+  Post.find({},(err,posts)=>{
+    res.render("home", {
+      content: homeStartingContent,
+      posts: posts
   })
+
+  })
+
+
+  
 
 
 })
@@ -41,28 +64,40 @@ app.get("/compose", (req, res) => {
 
 app.post("/compose", (req, res) => {
   let title = req.body.postTitle;
-  let body = req.body.postBody;
-  const post = {
+  let content = req.body.postBody;
+  const post =new Post ({
     title: title,
-    body: body
-  }
-  posts.push(post)
-  res.redirect("/")
+    content: content
+  })
+  // posts.push(post)
+  post.save((err)=>{
+    if(!err){
+      res.redirect("/");
+    }
+  });
+  
 
 
 })
-app.get("/posts/:title", (req, res) => {
+app.get("/posts/:postId", (req, res) => {
   let title = req.params.title;
-  posts.forEach((post) => {
-    if (_.lowerCase(post.title) === _.lowerCase(title)) {
-      res.render("post", {
-        post: post.title,
-        body: post.body
-      })
-    } else {
-      console.log("Nothing with that title found");
-    }
+  const requestedPostId=req.params.postId;
+  // posts.forEach((post) => {
+  //   if (_.lowerCase(post.title) === _.lowerCase(title)) {
+  //     res.render("post", {
+  //       post: post.title,
+  //       body: post.body
+  //     })
+  //   } else {
+  //     console.log("Nothing with that title found");
+  //   }
 
+  // })
+  Post.findOne({_id:requestedPostId},function(err,post){
+    res.render("post",{
+      title:post.title,
+      content:post.content
+    })
   })
 })
 
